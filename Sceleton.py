431b38f2f33c6f70d84dfb64f0d0d1f37ddf4ff1
Anastasiia-Pavlushenko <anastasiia.pavlushenko@nure.ua>
2018-10-18 16:53:12 +0300

    Create Sceleton

--- Sceleton.py
+++ Sceleton.py
@@ -2,6 +2,7 @@ import pygame
 BLACK = (0, 0, 0)
 pygame.init()
 
+
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Platformer Game")
 clock = pygame.time.Clock()
@@ -10,15 +11,18 @@ running = True
 while running:
     clock.tick(60)
 
+    #Цикл обработки событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
+    #Обновление состояни тгровых объектов
     pass
 
-
+    #Отрисовка
     screen.fill(BLACK)
     pygame.display.flip()
 
+
 pygame.quit()
+
